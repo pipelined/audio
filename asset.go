ead43c1af5a882c8622bec4113efd8ce9a0f2851
Sergei Dudka <serg.dudk@gmail.com>
2020-08-10 16:17:50 +0200

    add asset preallocation

--- asset.go
+++ asset.go
@@ -11,38 +11,78 @@ import (
 // It can be used to slice signals and use it for processing input.
 type Asset struct {
 	signal.SampleRate
-	signal.Floating
+	Signal signal.Floating
 }
 
-// Source implements clip source with a data from the asset.
-func (a Asset) Source(start, end int) pipe.SourceAllocatorFunc {
+// Source implements signal source for any signal type.
+func Source(sr signal.SampleRate, s signal.Signal) pipe.SourceAllocatorFunc {
 	return func(bufferSize int) (pipe.Source, pipe.SignalProperties, error) {
 		return pipe.Source{
-				SourceFunc: assetSource(a.Floating.Slice(start, end)),
+				SourceFunc: signalSource(s),
 			}, pipe.SignalProperties{
-				Channels:   a.Channels(),
-				SampleRate: a.SampleRate,
+				Channels:   s.Channels(),
+				SampleRate: sr,
 			}, nil
 	}
 }
 
-func assetSource(data signal.Floating) pipe.SourceFunc {
+func signalSource(s signal.Signal) pipe.SourceFunc {
+	switch v := s.(type) {
+	case signal.Signed:
+		return signedSource(v)
+	case signal.Unsigned:
+		return unsignedSource(v)
+	case signal.Floating:
+		return floatingSource(v)
+	}
+	panic("should never happen")
+}
+
+func floatingSource(data signal.Floating) pipe.SourceFunc {
 	pos := 0
 	return func(out signal.Floating) (int, error) {
-		if pos == data.Len() {
+		if pos == data.Length() {
 			return 0, io.EOF
 		}
-		end := pos + out.Len()
-		if end > data.Len() {
-			end = data.Len()
+		end := pos + out.Length()
+		if end > data.Length() {
+			end = data.Length()
 		}
-		read := 0
-		for pos < end {
-			out.SetSample(read, data.Sample(pos))
-			read++
-			pos++
+		read := signal.FloatingAsFloating(data.Slice(pos, end), out)
+		pos += read
+		return read, nil
+	}
+}
+
+func signedSource(data signal.Signed) pipe.SourceFunc {
+	pos := 0
+	return func(out signal.Floating) (int, error) {
+		if pos == data.Length() {
+			return 0, io.EOF
 		}
-		return read / data.Channels(), nil
+		end := pos + out.Length()
+		if end > data.Length() {
+			end = data.Length()
+		}
+		read := signal.SignedAsFloating(data.Slice(pos, end), out)
+		pos += read
+		return read, nil
+	}
+}
+
+func unsignedSource(data signal.Unsigned) pipe.SourceFunc {
+	pos := 0
+	return func(out signal.Floating) (int, error) {
+		if pos == data.Length() {
+			return 0, io.EOF
+		}
+		end := pos + out.Length()
+		if end > data.Length() {
+			end = data.Length()
+		}
+		read := signal.UnsignedAsFloating(data.Slice(pos, end), out)
+		pos += read
+		return read, nil
 	}
 }
 
@@ -50,13 +90,15 @@ func assetSource(data signal.Floating) pipe.SourceFunc {
 func (a *Asset) Sink() pipe.SinkAllocatorFunc {
 	return func(bufferSize int, props pipe.SignalProperties) (pipe.Sink, error) {
 		a.SampleRate = props.SampleRate
-		a.Floating = signal.Allocator{
-			Channels: props.Channels,
-			Capacity: bufferSize,
-		}.Float64()
+		if a.Signal == nil {
+			a.Signal = signal.Allocator{
+				Channels: props.Channels,
+				Capacity: bufferSize,
+			}.Float64()
+		}
 		return pipe.Sink{
 			SinkFunc: func(in signal.Floating) error {
-				a.Floating = a.Floating.Append(in)
+				a.Signal = a.Signal.Append(in)
 				return nil
 			},
 		}, nil
