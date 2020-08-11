5571bc4da047a6b9e87204e6f38cacf55d0657a2
Sergei Dudka <serg.dudk@gmail.com>
2020-08-11 21:07:05 +0200

    draft implementation of generic asset

--- asset.go
+++ asset.go
@@ -1,14 +1,16 @@
 package audio
 
 import (
+	"context"
+
 	"pipelined.dev/pipe"
 	"pipelined.dev/signal"
 )
 
-// Asset is a sink which uses a regular buffer as underlying storage.
-// It can be used to slice signals and use it for processing input.
+// Asset is a sink which uses a regular buffer as underlying storage. It
+// can be used to slice signal data and use it as processing input. Buffer type can
 type Asset struct {
-	Signal     signal.Floating
+	signal.Signal
 	sampleRate signal.SampleRate
 }
 
@@ -17,19 +19,90 @@ func (a *Asset) SampleRate() signal.SampleRate {
 	return a.sampleRate
 }
 
-// Sink appends buffers to asset.
+// Sink uses signal.Floating buffer to store signal data.
 func (a *Asset) Sink() pipe.SinkAllocatorFunc {
+	switch a.Signal.(type) {
+	case signal.Signed:
+		return a.sinkSigned()
+	case signal.Unsigned:
+	default:
+		return a.sinkFloating()
+	}
+	return nil
+}
+
+func (a *Asset) sinkFloating() pipe.SinkAllocatorFunc {
 	return func(bufferSize int, props pipe.SignalProperties) (pipe.Sink, error) {
 		a.sampleRate = props.SampleRate
-		if a.Signal == nil {
-			a.Signal = signal.Allocator{
-				Channels: props.Channels,
-				Capacity: bufferSize,
-			}.Float64()
-		}
+		data := floatingAsset(a.Signal, props.Channels, bufferSize)
 		return pipe.Sink{
 			SinkFunc: func(in signal.Floating) error {
-				a.Signal = a.Signal.Append(in)
+				data = data.Append(in)
+				return nil
+			},
+			FlushFunc: func(context.Context) error {
+				a.Signal = data
+				return nil
+			},
+		}, nil
+	}
+}
+
+// floatingAsset returns preallocated bufer if provided otherwise allocates new.
+func floatingAsset(s signal.Signal, channels, bufferSize int) signal.Floating {
+	if s != nil {
+		return s.(signal.Floating)
+	}
+	return signal.Allocator{
+		Channels: channels,
+		Capacity: bufferSize,
+	}.Float64()
+}
+
+func (a *Asset) sinkSigned() pipe.SinkAllocatorFunc {
+	return func(bufferSize int, props pipe.SignalProperties) (pipe.Sink, error) {
+		a.sampleRate = props.SampleRate
+		data := a.Signal.(signal.Signed)
+		// increment buffer is used only to grow the capacity of the data slice
+		inc := signal.Allocator{
+			Channels: props.Channels,
+			Capacity: bufferSize,
+			Length:   bufferSize,
+		}.Int8(signal.MaxBitDepth)
+		pos := 0
+		return pipe.Sink{
+			SinkFunc: func(in signal.Floating) error {
+				data = data.Append(inc)
+				pos += signal.FloatingAsSigned(in, data.Slice(pos, pos+bufferSize))
+				return nil
+			},
+			FlushFunc: func(context.Context) error {
+				a.Signal = data
+				return nil
+			},
+		}, nil
+	}
+}
+
+func (a *Asset) sinkUnsigned() pipe.SinkAllocatorFunc {
+	return func(bufferSize int, props pipe.SignalProperties) (pipe.Sink, error) {
+		a.sampleRate = props.SampleRate
+		data := a.Signal.(signal.Unsigned)
+		// increment buffer is used only to grow the capacity of the data slice
+		inc := signal.Allocator{
+			Channels: props.Channels,
+			Capacity: bufferSize,
+			Length:   bufferSize,
+		}.Uint8(signal.MaxBitDepth)
+		pos := 0
+		return pipe.Sink{
+			SinkFunc: func(in signal.Floating) error {
+				data = data.Append(inc)
+				pos += signal.FloatingAsUnsigned(in, data.Slice(pos, pos+bufferSize))
+				return nil
+			},
+			FlushFunc: func(context.Context) error {
+				a.Signal = data
 				return nil
 			},
 		}, nil
